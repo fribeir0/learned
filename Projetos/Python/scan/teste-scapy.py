3a36acc11185898e7818904a12d88e54478215b7
Felipe Ribeiro de Carvalho <felipe.carvalho@grupomix.local>
2025-03-28 16:52:39 -0300

    python scan v000.01

--- Projetos/Python/scan/teste-scapy.py
+++ Projetos/Python/scan/teste-scapy.py
@@ -3,6 +3,12 @@ from scapy.all import ARP, Ether, srp, IP, ICMP, sr1
 
 from concurrent.futures import ThreadPoolExecutor
 
+hc = 0
+linux_count = 0
+microsoft_count = 0
+unknown_count = 0
+
+
 def scan_devices(target):
     broadcast = "ff:ff:ff:ff:ff:ff"
     packet_arp = ARP(pdst=target)
@@ -17,43 +23,49 @@ def scan_devices(target):
         yield device_info 
 
 def scan(network):
-    answer=int (input("Deseja realizar um scan de portas SSH e RDP?\n 1- SIM\n2- NAO"))
-    with ThreadPoolExecutor(max_workers=20) as executor: 
-        targets = [f"{network}.{i}" for i in range(1, 255)]
+    global hc, linux_count, microsoft_count, unknown_count
+    answer=int (input("Deseja realizar um scan de portas SSH e RDP?\n 1- SIM\n 2- NAO\n"))
+    if answer <= 2:
+        with ThreadPoolExecutor(max_workers=20) as executor: 
+            targets = [f"{network}.{i}" for i in range(1, 255)]
+            
+
+            results = executor.map(scan_devices, targets)
+            
         
-
-        results = executor.map(scan_devices, targets)
-        
-     
-        for result in results:
-            for device in result:
-                print(f"Dispositivo encontrado - IP: {device['ip']}, MAC: {device['mac']}")
-                pacote_icmp= IP(dst=device["ip"]) / ICMP()
-                result_icmp= sr1(pacote_icmp, timeout=1, verbose=False)
-
-                if result_icmp :
-                    endpoint = True
-                    print (f"Host Ativo ")
-                    print (f"TTL Do endpoint e: {result_icmp[IP].ttl}")
-                    ttl = result_icmp[IP].ttl
-                    if ttl > 64 :
-                        endpoint_so="Windows"
-                        print (f"Windows TTL")
+            for result in results:
+                for device in result:
+                    print(f"Dispositivo encontrado - IP: {device['ip']}, MAC: {device['mac']}")
+                    pacote_icmp= IP(dst=device["ip"]) / ICMP()
+                    result_icmp= sr1(pacote_icmp, timeout=1, verbose=False)
+                    
+                    if result_icmp :
+                        endpoint = True
+                        hc += 1
+                        print (f"Host Ativo ")
+                        print (f"TTL Do endpoint e: {result_icmp[IP].ttl}")
+                        ttl = result_icmp[IP].ttl
+                        if ttl > 64 :
+                            endpoint_so="Windows"
+                            print (f"Windows TTL")
+                            microsoft_count += 1
+                        else :
+                            endpoint_so="Linux"
+                            print (f"Linux TTL")
+                            linux_count += 1
                     else :
-                        endpoint_so="Linux"
-                        print (f"Linux TTL")
-                else :
-                    print (f"Inativo ou ICMP bloquado")
-                    endpoint = False
+                        print (f"Inativo ou ICMP bloquado")
+                        endpoint = False
+                        unknown_count += 1
 
-                if answer == 1:
-                    ports = {22,3389}
-                    open_ports = check_remote(device["ip"],ports)
-                    if open_ports:
-                        print (f"Portas Abertas em dispositivo: {device['ip']} {open_ports}")
-                        
-                    else :
-                        print (f"Nenhuma porta aberta")
+                    if answer == 1:
+                        ports = {22,3389}
+                        open_ports = check_remote(device["ip"],ports)
+                        if open_ports:
+                            print (f"Portas Abertas em dispositivo: {device['ip']} {open_ports}")
+                            
+                        else :
+                            print (f"Nenhuma porta aberta")
 
 
 def check_remote (ip, ports):
@@ -72,4 +84,18 @@ def check_remote (ip, ports):
     return opens
 
 network = "192.168.0"
-scan(network)
+
+try:
+    scan(network)
+except KeyboardInterrupt:
+
+    print (f"Host Ativos: {hc}")
+    print (f"Linux: {linux_count}")
+    print (f"Microsoft: {microsoft_count}")
+    print (f"Desconhecidos {unknown_count}")
+
+print (f"Host Ativos: {hc}")
+print (f"Linux: {linux_count}")
+print (f"Microsoft: {microsoft_count}")
+print (f"Desconhecidos: {unknown_count}")
+
