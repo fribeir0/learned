a44601547cd4d47e13d1233b0153496574e9364e
FELIPE RIBEIRO DE CARVALHO <46394858838@info.local>
2025-04-05 11:53:35 -0300

    task features

---
+++ Projetos/Python/tasks/celsius.py
@@ -0,0 +1,8 @@
+def convertC():
+    print("Digite o valor a ser convertido em Celsius para Fahrenheit")
+    value = float(input(""))
+    Fah = (value*1.8)+ 32
+    print (Fah)
+
+if __name__ == "__main__":
+    convertC()
---
+++ Projetos/Python/tasks/par.py
@@ -0,0 +1,7 @@
+def oi () :
+    name = input ("Digitie o seu nome: ")
+    idade = input ("Digite sua idade: ")
+    print (name,idade)
+
+if __name__ == "__main__" :
+    oi()
